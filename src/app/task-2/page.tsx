7f67e30c137958755f9aa9adffcfc1060ec9f185
smohammadhn <smohammadhnd@gmail.com>
2024-12-06 02:39:49 -0500

    task one done

---
+++ src/app/task-2/page.tsx
@@ -0,0 +1,15 @@
+/*
+Task Description:
+
+Create a very simple page with a header, a body text, and a footer.
+
+1. Header must be centered in the top of the page.
+1. Footer must be centered in the bottom of the page.
+3. Body text should be centered in the middle of the page.
+4. Please use Tailwind CSS for styling.
+
+Time estimation: 6 mins
+
+*/
+
+// CODE HERE: 👇
