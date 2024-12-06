4ac18df55d201b59b64b30a2312f7cc54399687f
smohammadhn <smohammadhnd@gmail.com>
2024-12-06 03:10:59 -0500

    task three done

---
+++ src/app/task-3/page.tsx
@@ -0,0 +1,49 @@
+/*
+Task Description:
+
+Put a background for the div containing the lorem ipsum text.
+
+*. DO NOT use css url() please
+*. use the image from this path: /public/images/background.jpg
+
+Time estimation: 5 mins
+
+*/
+
+export default function PageTaskThree() {
+  return (
+    <main className='p-10 text-center'>
+      <div>
+        <p className='font-bold p-4'>
+          Lorem ipsum dolor, sit amet consectetur adipisicing elit.
+          Exercitationem quidem, repudiandae excepturi doloribus corporis saepe
+          tempore impedit illo similique. Dignissimos, temporibus illo!
+          Consectetur sequi minus officia repudiandae sed eum doloremque in,
+          itaque magnam voluptatum. Necessitatibus, facilis veritatis ab, sit
+          incidunt voluptas hic rem quo, numquam laboriosam ipsam sunt est minus
+          quis pariatur dicta nemo corrupti atque et illo voluptatem omnis
+          deserunt. Itaque ratione quisquam dignissimos, possimus molestias
+          beatae saepe nulla atque temporibus ipsum reprehenderit sapiente
+          nesciunt quis. Ad numquam magni repellat, quas suscipit mollitia
+          dignissimos officia, dolores rem fugiat vel assumenda laborum
+          explicabo enim eius molestias voluptas in! Voluptates, hic!
+          <br />
+          <br />
+          Lorem ipsum dolor, sit amet consectetur adipisicing elit.
+          Exercitationem quidem, repudiandae excepturi doloribus corporis saepe
+          tempore impedit illo similique. Dignissimos, temporibus illo!
+          Consectetur sequi minus officia repudiandae sed eum doloremque in,
+          itaque magnam voluptatum. Necessitatibus, facilis veritatis ab, sit
+          incidunt voluptas hic rem quo, numquam laboriosam ipsam sunt est minus
+          quis pariatur dicta nemo corrupti atque et illo voluptatem omnis
+          deserunt. Itaque ratione quisquam dignissimos, possimus molestias
+          beatae saepe nulla atque temporibus ipsum reprehenderit sapiente
+          nesciunt quis. Ad numquam magni repellat, quas suscipit mollitia
+          dignissimos officia, dolores rem fugiat vel assumenda laborum
+          explicabo enim eius molestias voluptas in! Voluptates, hic!
+        </p>
+        {/* CODE HERE: 👇 */}
+      </div>
+    </main>
+  );
+}
